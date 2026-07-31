0dcb1d541d34642761b4538781cdc0d9bb7c142a
Alix <lelievre.alix@gmail.com>
2026-07-31 15:55:03 -0400

    feat: enable concurrent formatting for improved performance and add error handling

--- scripts/format.sh
+++ scripts/format.sh
@@ -13,29 +13,54 @@ mapfile -d '' shell_files < <(git ls-files --cached --others --exclude-standard 
 mapfile -d '' template_files < <(git ls-files --cached --others --exclude-standard -z -- '*.tmpl')
 mapfile -d '' toml_files < <(git ls-files --cached --others --exclude-standard -z -- '*.toml')
 
+formatter_pids=()
+formatter_names=()
+
 if ((${#go_files[@]})); then
     printf 'Formatting %d Go files with gofmt...\n' "${#go_files[@]}"
-    gofmt -w "${go_files[@]}"
+    gofmt -w "${go_files[@]}" &
+    formatter_pids+=("$!")
+    formatter_names+=("gofmt")
 fi
 
 if ((${#markdown_files[@]})); then
     printf 'Formatting %d Markdown files with rumdl...\n' "${#markdown_files[@]}"
-    rumdl fmt "${markdown_files[@]}"
+    rumdl fmt --no-cache "${markdown_files[@]}" &
+    formatter_pids+=("$!")
+    formatter_names+=("rumdl")
 fi
 
 if ((${#shell_files[@]})); then
     printf 'Formatting %d shell files with shfmt...\n' "${#shell_files[@]}"
-    shfmt -w -i 4 "${shell_files[@]}"
+    shfmt -w -i 4 "${shell_files[@]}" &
+    formatter_pids+=("$!")
+    formatter_names+=("shfmt")
 fi
 
 if ((${#template_files[@]})); then
     printf 'Formatting %d template files with gotmplfmt...\n' "${#template_files[@]}"
-    gotmplfmt -w "${template_files[@]}"
+    gotmplfmt -w "${template_files[@]}" &
+    formatter_pids+=("$!")
+    formatter_names+=("gotmplfmt")
 fi
 
 if ((${#toml_files[@]})); then
     printf 'Formatting %d TOML files with taplo...\n' "${#toml_files[@]}"
-    taplo fmt "${toml_files[@]}"
+    taplo fmt "${toml_files[@]}" &
+    formatter_pids+=("$!")
+    formatter_names+=("taplo")
+fi
+
+format_status=0
+for formatter_index in "${!formatter_pids[@]}"; do
+    if ! wait "${formatter_pids[$formatter_index]}"; then
+        printf '%s failed.\n' "${formatter_names[$formatter_index]}" >&2
+        format_status=1
+    fi
+done
+
+if ((format_status != 0)); then
+    exit "$format_status"
 fi
 
 end_time_ms=$(date +%s%3N)
